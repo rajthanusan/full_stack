79ecca6956d8466fa5ba2edc69c4b8e3beaf08b0
rajthanusan <rajthanusan08@gmail.com>
2024-03-05 22:53:33 +0530

    added

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CURD APPLICATION</h1>
+      <div className="form">
+        <input type="text" name="movieName" />
+        <input type="text" name="review" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
