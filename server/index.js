9c590f0d5696a5c5271a0d1dc73b673077571a2e
rajthanusan <rajthanusan08@gmail.com>
2024-03-07 09:03:53 +0530

    sql check code added

--- server/index.js
+++ server/index.js
@@ -5,18 +5,27 @@ const mysql = require("mysql");
 const db = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "password",
-  database: "CRUDDataBase",
+  password: "1234",
+  database: "cruddatabase",
 });
 
 app.get("/", (req, res) => {
   const sqlInsert =
-    "INSERT INTO movie_reviews (movieName,movieReview) VALUES ('inception','good movie');";
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('Inception', 'Good movie');";
   db.query(sqlInsert, (err, result) => {
-    res.send("file added");
+    if (err) {
+      console.error("Error executing SQL query:", err);
+      res
+        .status(500)
+        .send("An error occurred while inserting into the database.");
+        
+    } else {
+      console.log("1 record inserted");
+      res.send("File added successfully.");
+    }
   });
 });
 
 app.listen(3001, () => {
-  console.log("running on port 3001");
+  console.log("Running on port 3001");
 });
